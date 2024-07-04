4af6598f6a59d3c1d07f8a8010db513638cdd4c0
tans <tans@imac.local>
2024-07-04 16:48:25 +0800

    -

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="m-6 max-w-screen-md mx-auto">
       Ton Store is an e-commerce platform that helps people sell digital
       products, tokens, and memberships online. We accept payments in TON and
       USDT tokens.
